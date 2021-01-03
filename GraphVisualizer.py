2f670b3b20ae75509d898c1e58215d5fc82d461b
George Glidden-Handgis <gliddenegeorge@gmail.com>
2021-01-02 22:15:49 -0700

    inpr: bugfixing Surface2D algorithm w/ to support concave graphs

--- GraphVisualizer.py
+++ GraphVisualizer.py
@@ -1,69 +1,114 @@
-from SurfaceToShape import EmbeddedGraph
+from SurfaceToShape import EmbodiedGraph, Surface2D_momentum
 import sys, re
 from random import randint
 tupler = lambda edge: re.sub(r'[()]', '', edge).split(',')
 to_ints = lambda x: (int(x[0]), int(x[1]))
 parse = lambda edge: to_ints(tupler(edge))
 try:
-    with open(sys.argv[1], 'r') as graph_str:
-        data = [line.replace('\n', '') for line in graph_str]
-        V = int(data[0])
-        D = int(data[1])
-        E = int(data[2+V])
-        embedding = [parse(embed) for embed in data[2:2+V]]
-        edges = [parse(edge) for edge in data[3+V:3+V+E]]
-        G = EmbeddedGraph(V, D, embedding, edges)
-        assert G.E() == E
+    G = None
+    ext = sys.argv[1][-4:]
+    if ext == '.txt':
+        with open(sys.argv[1], 'r') as graph_str:
+            data = [line.replace('\n', '') for line in graph_str]
+            V = int(data[0])
+            D = int(data[1])
+            E = int(data[2+V])
+            embedding = [parse(embed) for embed in data[2:2+V]]
+            edges = [parse(edge) for edge in data[3+V:3+V+E]]
+            G = EmbodiedGraph(V, D, embedding, edges)
+            assert G.E() == E
+    elif ext == '.png':
+        from skimage.io import imread
+        from skimage.filters import threshold_otsu
+        blobsource = imread(sys.argv[1], as_gray=True)
+        D = len(blobsource.shape)
+        assert D == 2
+        w, h = blobsource.shape
+        print(w,h)
+        is_node = blobsource > threshold_otsu(blobsource)
+        nbhd_rule = int(input('4-adj or 8-adj? (enter an integer): '))
+        if nbhd_rule == 4:
+            adj = lambda x, y: [(x-1,y),(x,y-1),(x+1,y),(x,y+1)]
+        elif nbhd_rule == 8:
+            adj = lambda x, y: [(x-1,y),(x-1,y-1),(x,y-1),(x+1,y-1),(x+1,y),(x+1,y+1),(x,y+1),(x-1,y+1)]
+        inv_emb = dict()
+        V = 0
+        for x in range(w):
+            for y in range(h):
+                if is_node[x,y] == 1:
+                    inv_emb[(x,y)] = V
+                    V += 1
+        edges = []
+        embedding = list(inv_emb.keys())
+        for v in range(V):
+            x1,y1 = embedding[v]
+            for x2,y2 in adj(x1,y1):
+                if is_node[x2,y2]:
+                    w = inv_emb[x2,y2]
+                    if v < w:   edges.append((v,w))
+        G = EmbodiedGraph(V, D, embedding, edges)
 except FileNotFoundError:
     print(f'the file {sys.argv[1]} could be located.')
-except:
-    print(f'failed to load graph from {sys.argv[1]}.')
+
+#except Exception as ex:
+#    print(f'failed to load graph from {sys.argv[1]}.')
+#    print(ex)
 else:
     print(
         f'loaded an embedded graph from {sys.argv[1]}.\n'
         f'V: {G.V()}\tE: {G.E()}\tD: {G.D()}')
 
-print('\ninitializing ux...')
-import pygame
-pygame.init()
-w,h = int(sys.argv[2]), int(sys.argv[3])
-screen = pygame.display.set_mode((w,h))
+    surface = Surface2D_momentum(G)
 
-pad = float(sys.argv[4])
-x_min, x_max = min(embedding, key=lambda e:e[0]), max(embedding, key=lambda e:e[0])
-G_w = x_max[0] - x_min[0]
-x_offset = w * pad
-x_scale = (w - (2*x_offset)) / G_w
-y_min, y_max = min(embedding, key=lambda e:e[1]), max(embedding, key=lambda e:e[1])
-G_h = y_max[1] - y_min[1]
-y_offset = h * pad
-y_scale = (h - (2*y_offset)) / G_h
-points = [(x_offset + x_scale * x, y_offset + y_scale * y) for (x,y) in embedding]
-print(
-    f'\nrescaled graph embedding to fit screen {w,h} with {100*pad}% padding.\n'
-    f'original: {embedding}\n'
-    f'rescaled: {points}')
+    print('\ninitializing ux...')
+    import pygame
+    pygame.init()
+    w,h = int(sys.argv[2]), int(sys.argv[3])
+    screen = pygame.display.set_mode((w,h))
 
-dot_size, line_size = int(sys.argv[5]), int(sys.argv[6])
-vtx_color =  [pygame.Color(randint(0,200),randint(0,200),randint(0,200)) for v in range(G.V())]
-edge_color = pygame.Color(30,30,30)
-bg_color = pygame.Color(255,255,255)
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            print('stopping ux...')
-            pygame.quit()
-            print('goodbye')
-            sys.exit()
-    screen.fill(bg_color)
-    for v in range(G.V()):
-        x1,y1 = points[v]
-        for w in G.adj(v):
-            if v > w:
-                x2,y2 = points[w]
-                pygame.draw.line(screen, edge_color, (x1,y1), (x2,y2), line_size)
-    for v in range(G.V()):
-        x,y = points[v]
-        color = vtx_color[v]
-        pygame.draw.circle(screen, color, (x,y), dot_size)
-    pygame.display.update()
+    pad = float(sys.argv[4])
+    x_min, x_max = min(embedding, key=lambda e:e[0]), max(embedding, key=lambda e:e[0])
+    G_w = x_max[0] - x_min[0]
+    x_offset = w * pad
+    x_scale = (w - (2*x_offset)) / G_w
+    y_min, y_max = min(embedding, key=lambda e:e[1]), max(embedding, key=lambda e:e[1])
+    G_h = y_max[1] - y_min[1]
+    y_offset = h * pad
+    y_scale = (h - (2*y_offset)) / G_h
+    scale_embedding = lambda x, y : (x_offset + x_scale * (x - x_min[0]), y_offset + y_scale * (y - y_min[1]))
+    points = [scale_embedding(x,y) for (x,y) in embedding]
+    center_point = scale_embedding(G.pos_c()[0], G.pos_c()[1])
+    print(
+        f'\nrescaled graph embedding to fit screen {w,h} with {100*pad}% padding.\n'
+        f'G_w: {G_w} G_h: {G_h} x_s: {x_scale} y_s: {y_scale} y_o: {y_offset} x_o: {x_offset} \n'
+        f'original: {embedding[:min(G.V(), 20)]}\n'
+        f'rescaled: {points[:min(G.V(), 20)]}')
+
+    dot_size, line_size = int(sys.argv[5]), int(sys.argv[6])
+    vtx_color =  [pygame.Color(100,50,randint(0,50)) for v in range(G.V())]
+    for i in range(surface.perimeter()):
+        v = surface.vertices()[i]
+        vtx_color[v] = pygame.Color(0, 100+int(155 * i/surface.perimeter()), 100+int(155 * (surface.perimeter() - i)/surface.perimeter()))
+    center_color = pygame.Color(255, 205, 30)
+    edge_color = pygame.Color(30,30,30)
+    bg_color = pygame.Color(255,255,255)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print('stopping ux...')
+                pygame.quit()
+                print('goodbye')
+                sys.exit()
+        screen.fill(bg_color)
+        pygame.draw.circle(screen, center_color, center_point, 2*dot_size)
+        for v in range(G.V()):
+            x1,y1 = points[v]
+            for w in G.adj(v):
+                if v > w:
+                    x2,y2 = points[w]
+                    pygame.draw.line(screen, edge_color, (x1,y1), (x2,y2), line_size)
+        for v in range(G.V()):
+            x,y = points[v]
+            color = vtx_color[v]
+            pygame.draw.circle(screen, color, (x,y), dot_size)
+        pygame.display.update()
--- SurfaceToShape.py
+++ SurfaceToShape.py
@@ -1,5 +1,5 @@
 from MPC import Graph
-from numpy import array, argmin, argmax, sqrt, sum, power, arctan2
+from numpy import array, argmin, argmax, sqrt, sum, power, arctan2, pi as PI
 from numpy.fft import fft
 
 # symmetric distance between x1 and x2
@@ -59,39 +59,42 @@ class EmbodiedGraph(EmbeddedGraph):
             e = self.pos(v)
             for d in range(self.D()):
                 esum[d] += e[d]
-        pos_c = tuple([esum[d]/wsum for d in range(self.D())])
-        self._c = argmin([dist(self.pos(v), pos_c) for v in range(self.V())])
+        self._pos_c = tuple([esum[d]/wsum for d in range(self.D())])
+        self._c = argmin([dist(self.pos(v), self._pos_c) for v in range(self.V())])
 
     def mass(self, v):
         return self._mass(v)
 
-    def c(self):
+    def pos_c(self):
+        return self._pos_c
+
+    def vtx_c(self):
         return self._c
 
     def dist(self, v, w=None):
         if w == None:
-            return super().dist(v, self.c())
+            return self._dist(self.pos(v), self.pos_c())
         else:
             return super().dist(v, w)
 
     def angle(self, v, w=None):
         if w == None:
-            return super().angle(v, self.c())
+            return self._angle(self.pos(v), self.pos_c())
         else:
             return super().angle(v, w)
 
 # perimeter of a planar graph
 class Surface2D:
-    def __init__(self, embodied_graph):
+    def __init__(self, embodied_graph, obj = None):
         self.G = embodied_graph
         self._label = [v for v in range(self.G.V())]
         # sort vertices by distance to the center of mass.
         vertices_by_distance = sorted([v for v in range(self.G.V())], key=lambda v: self.G.dist(v))
         # the vertex furthest from the center must lie on the surface,
         f = vertices_by_distance[-1]
-        # the idx of a vertex in vertices_by_distance is a heuristic for its distance to the center.
-        relative_dist = {vertices_by_distance[i] : i for i in range(self.G.V())}
-        # so, we can use a simple maximization scheme to iteratively construct our perimeter cycle.
+        if obj == None:
+            # use a simple maximization scheme to iteratively construct our perimeter cycle.
+            obj = self.G.dist
         tail = f
         head = f
         self._label[head] = self._label[tail]
@@ -99,14 +102,13 @@ class Surface2D:
         while self._hom_deg(tail) < 2 and len(cycle) < self.G.V():
             unvisited = self._het_adj(head)
             cycle.append(head)
-            head = unvisited[argmax([relative_dist[v] for v in unvisited])]
+            head = unvisited[argmax([obj(v) for v in unvisited])]
             self._label[head] = self._label[tail]
         cycle.append(head)
         # the surface is a cycle ...
         assert cycle[0] in self._hom_adj(cycle[-1])
-        # ... which should comprise the n nodes furthest from the center of mass
         n = len(cycle)
-        assert set(cycle) == set(vertices_by_distance[-n:])
+        #assert set(cycle) == set(vertices_by_distance[-n:])
 
         self._surface = cycle
         self._n = n
@@ -133,6 +135,9 @@ class Surface2D:
     def vertices(self):
         return self._surface
 
+    def points(self):
+        return [self.G.pos(v) for v in self.vertices()]
+
     def distance_sequence(self):
         return [self.G.dist(v) for v in self.vertices()]
 
@@ -140,13 +145,44 @@ class Surface2D:
         seq = self.distance_sequence()
         return fft(seq) / self.perimeter()
 
-'''
-        # find a cycle -> store as linked list
-        # iterate cycle 3 at a time checking
+class Surface2D_momentum(Surface2D):
+    def __init__(self, embodied_graph, sustain = 40):
+        self.G = embodied_graph
+        self._label = [v for v in range(self.G.V())]
+        vertices_by_distance = sorted([v for v in range(self.G.V())], key=lambda v: self.G.dist(v))
+        f = vertices_by_distance[-1]
+        tail = f
+        head = f
+        self._label[head] = self._label[tail]
+        cycle = []
+        while self._hom_deg(tail) < 2 and len(self._het_adj(head)) > 0:
+            cycle.append(head)
+            trail = cycle[-sustain:]
+            t = len(trail)
+            delta_angle = lambda v, w: self.G.angle(w) - self.G.angle(v)
+            adjusted_delta_angle = lambda v, w: min(delta_angle(v, w), 2*PI - delta_angle(v,w))
+            if t > 0:
+                sum_delta_angle = 0.0
+                for v in range(t-1):
+                    w = v+1
+                    sum_delta_angle += adjusted_delta_angle(v,w)
+                avg_delta_angle = sum_delta_angle / t
+                # momentum objective
+                #obj = lambda v: self.G.dist(v) - (sustain * abs(avg_delta_angle - delta_angle(head, v)))
+            obj = lambda v: self.G.dist(v) - (abs(avg_delta_angle - adjusted_delta_angle(head, v)))
+            unvisited = self._het_adj(head)
+            head = unvisited[argmax([obj(v) for v in unvisited])]
+            self._label[head] = self._label[tail]
+            if t > 0:
+                print(f'{len(cycle)} {self.G.dist(head)}\t{abs(avg_delta_angle - adjusted_delta_angle(cycle[-1], head))}\t{self.G.dist(head) - (abs(avg_delta_angle - adjusted_delta_angle(cycle[-1], head)))}')
 
-    def dfs(self, s):
-        pass
-'''
+        cycle.append(head)
+        # the surface is a cycle
+        assert cycle[0] in self._hom_adj(cycle[-1])
+        n = len(cycle)
+
+        self._surface = cycle
+        self._n = n
 
 def main():
     import sys, re
@@ -154,22 +190,54 @@ def main():
     to_ints = lambda x: (int(x[0]), int(x[1]))
     parse = lambda edge: to_ints(tupler(edge))
     G = None
-    with open(sys.argv[1], 'r') as graph_str:
-        data = [line.replace('\n', '') for line in graph_str]
-        V = int(data[0])
-        D = int(data[1])
-        E = int(data[2+V])
-        embedding = [parse(embed) for embed in data[2:2+V]]
-        edges = [parse(edge) for edge in data[3+V:3+V+E]]
+    ext = sys.argv[1][-4:]
+    if ext == '.txt':
+        with open(sys.argv[1], 'r') as graph_str:
+            data = [line.replace('\n', '') for line in graph_str]
+            V = int(data[0])
+            D = int(data[1])
+            E = int(data[2+V])
+            embedding = [parse(embed) for embed in data[2:2+V]]
+            edges = [parse(edge) for edge in data[3+V:3+V+E]]
+            G = EmbodiedGraph(V, D, embedding, edges)
+            assert G.E() == E
+    elif ext == '.png':
+        from skimage.io import imread
+        from skimage.filters import threshold_otsu
+        blobsource = imread(sys.argv[1], as_gray=True)
+        D = len(blobsource.shape)
+        assert D == 2
+        w, h = blobsource.shape
+        print(w,h)
+        is_node = blobsource > threshold_otsu(blobsource)
+        nbhd_rule = 4
+        if nbhd_rule == 4:
+            adj = lambda x, y: [(x-1,y),(x,y-1),(x+1,y),(x,y+1)]
+        elif nbhd_rule == 8:
+            adj = lambda x, y: [(x-1,y),(x-1,y-1),(x,y-1),(x+1,y-1),(x+1,y),(x+1,y+1),(x,y+1),(x-1,y+1)]
+        inv_emb = dict()
+        V = 0
+        for x in range(w):
+            for y in range(h):
+                if is_node[x,y] == 1:
+                    inv_emb[(x,y)] = V
+                    V += 1
+        edges = []
+        embedding = list(inv_emb.keys())
+        for v in range(V):
+            x1,y1 = embedding[v]
+            for x2,y2 in adj(x1,y1):
+                if is_node[x2,y2]:
+                    w = inv_emb[x2,y2]
+                    if v < w:   edges.append((v,w))
         G = EmbodiedGraph(V, D, embedding, edges)
-        assert G.E() == E
     if G:
         center = G.c()
         print('center')
         print(center, G.pos(center), G.dist(center), G.angle(center))
         surface = Surface2D(G)
         print('surface')
-        print(surface.perimeter(), surface.vertices(), surface.distance_sequence(), surface.characteristic())
+        print(surface.perimeter(), surface.points(), surface.distance_sequence(), surface.characteristic())
 
 if __name__ == '__main__':
     main()
